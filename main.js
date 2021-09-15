e3cfb0c6e92fda8d38f489aa294c39f823fb7f4d
imfloors <daniel.lp.rosario@gmail.com>
2021-09-15 17:29:50 -0300

    update tab-panel products sale 15sept

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ $('#locales-list a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
   })
+  
+$('#prodcat-list a').on('click', function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
